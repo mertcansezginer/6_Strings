4210511c80b08c155d2b3e9a80408dbe2601fd34
mertc <mertc@DESKTOP-8ID65LJ>
2024-03-06 16:54:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+string metin = "Bugün disarida firtina var!";
+
+int uzunluk = metin.Length;
+Console.WriteLine("Metnin uzunlugu " + uzunluk);
+
+char ucuncuKarakter = metin[2];  //belirli karaktere eristik
+Console.WriteLine("Metnin ucuncu karakteri: " + ucuncuKarakter);
+
+string buyukHarf = metin.ToUpper();
+Console.WriteLine("Metnin buyuk harfli hali: " + buyukHarf);
+
+string kucukHarf = metin.ToLower();
+Console.WriteLine("Metnin kucuk harfli hali: " + kucukHarf);
+
+string kesilmisMetin = metin.Substring(6,8);  //metin belirli yerden kesildi, 6. karakterden itibaren 8 karakter alindi
+Console.WriteLine("Metnin kesilmis hali: " + kesilmisMetin);
+
+bool iceriyorMu = metin.Contains("firtina");
+Console.WriteLine("Metin 'firtina' kelimesini iceriyor mu? " + (iceriyorMu ? "Evet" : "Hayır")); //ternary operator kullandik
+
